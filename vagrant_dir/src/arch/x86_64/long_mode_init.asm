7ea6aa96da79dd87cc785b1afa961b6eccd3f710
Kyle Montgomery <kmontg003@gmail.com>
2017-02-12 14:09:36 -0500

    Linked with Rust

--- vagrant_dir/src/arch/x86_64/long_mode_init.asm
+++ vagrant_dir/src/arch/x86_64/long_mode_init.asm
@@ -11,8 +11,17 @@ long_mode_start:
     mov fs, ax
     mov gs, ax
 
-    ; print `OKAY` to screen
-    mov rax, 0x2f592f412f4b2f4f
-    mov qword [0xb8000], rax
+    ; call the rust main
+    extern rust_main
+    call rust_main
 
+
+    ; print `OKAY` to screen
+.os_returned:
+    mov rax, 0x4f724f204f534f4f
+    mov [0xb8000], rax
+    mov rax, 0x4f724f754f744f65
+    mov [0xb8008], rax
+    mov rax, 0x4f214f644f654f6e
+    mov [0xb8010], rax
     hlt
